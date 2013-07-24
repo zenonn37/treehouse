9b9f0b146843879140969b8fdb636026d8d96acb
chris crowd <zent29@gmail.com>
2013-07-24 12:09:28 -0400

    updated front end
    added simpleform
    updated several forms to bootstrap

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.13'
 gem 'bootstrap-sass','2.1'
 gem 'devise'
+gem 'simple_form'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,14 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
    attr_accessible :email, :password,:password_confirmation,:first_name,:last_name,:profile_name
+    
+    has_many :statuses
+   def fullName
+
+   	"#{first_name} #{last_name}"
+   end
 end
